40187f2e3b4bb2d84f4d44073588110b9b67a30a
cdlanauze <72032549+cdlanauze@users.noreply.github.com>
2022-01-17 10:01:33 -0500

    Add files via upload

--- const_hack.py
+++ const_hack.py
@@ -1,6 +1,6 @@
 
 STUDIUM_LINK=r'https://studium.umontreal.ca/'
 DRIVER_PATH='C:\Program Files (x86)\chromedriver1.exe'
-username='p1221508'
-password='Hiba123_'
+username=''
+password=''
 TO_DO_LINK=r'https://cdlanauze.github.io/'
